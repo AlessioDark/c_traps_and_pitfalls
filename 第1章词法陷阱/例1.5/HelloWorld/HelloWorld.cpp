f134c8087437e279deab255c19dfbd7411f03754
maoxq <3288868269@qq.com>
2021-02-10 16:05:45 +0800

    HelloWorld工程文件

---
+++ 第1章词法陷阱/例1.5/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,16 @@
+﻿// HelloWorld.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+    printf("Hello World\n");
+    char hello[] = {'H','e','l','l','o',' ','W','o','r','l','d','\n',0};
+
+    printf(hello);
+
+}
+
+
